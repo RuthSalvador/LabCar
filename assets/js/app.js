13a0c39931e1c635d8d84b8ff38c5edd54f72160
Ruth Salvador <ruth.abby.sz@gmail.com>
2017-05-29 15:35:34 -0500

    tarifa mobile and desktop init

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,11 @@
-
+function initMap() {
+  var yo = {lat: -25.363, lng: 131.044};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 18,
+    center: yo
+  });
+  var marker = new google.maps.Marker({
+    position: yo,
+    map: map
+  });
+}
